2dbb1a26549f8495458ac49be325b74b3c7a28f0
Maverick <maverickmailhot2@gmail.com>
2024-03-13 09:57:22 -0400

    First commit

---
+++ Calculator.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    char myOperator;
+    float num1, num2;
+    cout << "Enter operator: 1 = +, 2 = -, 3 = *, 4 = /" << endl;
+    cin >> myOperator;
+
+    cout << myOperator << endl;
+
+    return 0;
+}
+
+int result = main(); 
